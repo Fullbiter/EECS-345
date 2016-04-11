7cf609a5123be14a2c6268a505f17ee10585afd9
Kevin Nash <kjn33@case.edu>
2016-04-11 19:49:57 -0400

    add_to_back() finished, including edge case

--- PE3/hello.c
+++ PE3/hello.c
@@ -22,16 +22,26 @@ void add_to_front(void* data, list* ls)
     node* n = (node*)calloc(1, sizeof(node));   // allocate memory for the node
     
     n->data = data; // set the new node's data member to the data argument
-    if (ls->head != NULL)
-    {
-        n->next = ls->head; // point the new node forward to the old head
-        ls->head->prev = n; // point the old head backward to new node
-    }
-    else
-    {
+    n->next = ls->head; // point the new node forward to the old head
+    if (ls->head == NULL)
         ls->tail = n;   // set list's tail pointer to the new node
-    }
+    else
+        ls->head->prev = n; // point the old head backward to the new node
     ls->head = n;   // set list's head pointer to the new node
+}
+
+/* */
+void add_to_back(void* data, list* ls)
+{
+    node* n = (node*)calloc(1, sizeof(node));   // allocate memory for the node
+    
+    n->data = data; // set the new node's data member to the data argument
+    n->prev = ls->tail; // point the new node backward to the old tail
+    if (ls->head == NULL)
+        ls->head = n;   // set list's head pointer to the new node
+    else
+        ls->tail->next = n; // point the old tail forward to the new node
+    ls->tail = n;   // set list's tail pointer to the new node
 }
 
 /* */
@@ -52,8 +62,10 @@ int main()
     //ls->head = head;
     //ls->tail = tail;
 
-    add_to_front((int*)3, ls);
+
+    add_to_back((int*)3, ls);
+
     printf("%i, %i\n", ls->head->data, ls->tail->data);
-    //printf("%i, %i, %i\n", ls->head->data, ls->head->next->data, ls->tail->data);
+
     return(0);
 }
