23105196e8c8d34ac7646bb5f5addfc49437391e
Kevin Nash <kjn33@case.edu>
2016-04-11 19:25:17 -0400

    Fixed empty list edge case for add_to_front()

--- PE3/hello.c
+++ PE3/hello.c
@@ -21,11 +21,17 @@ void add_to_front(void* data, list* ls)
 {
     node* n = (node*)calloc(1, sizeof(node));   // allocate memory for the node
     
-    n->data = data;     // set the new node's data member to the data argument
-    n->next = ls->head;  // point the new node forward to the old head
-    
-    ls->head->prev = n;  // point the old head backward to new node
-    ls->head = n;        // set list's head pointer to the new node
+    n->data = data; // set the new node's data member to the data argument
+    if (ls->head != NULL)
+    {
+        n->next = ls->head; // point the new node forward to the old head
+        ls->head->prev = n; // point the old head backward to new node
+    }
+    else
+    {
+        ls->tail = n;   // set list's tail pointer to the new node
+    }
+    ls->head = n;   // set list's head pointer to the new node
 }
 
 /* */
@@ -43,11 +49,11 @@ int main()
     tail->prev = head;
     tail->next = NULL;
 
-    ls->head = head;
-    ls->tail = tail;
+    //ls->head = head;
+    //ls->tail = tail;
 
     add_to_front((int*)3, ls);
-
-    printf("%i, %i, %i\n", ls->head->data, ls->head->next->data, ls->tail->data);
+    printf("%i, %i\n", ls->head->data, ls->tail->data);
+    //printf("%i, %i, %i\n", ls->head->data, ls->head->next->data, ls->tail->data);
     return(0);
 }
