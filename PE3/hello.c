47b5e057c58e5237396b9cf4a14ef81094712d90
Kevin Nash <kjn33@case.edu>
2016-04-12 15:14:14 -0400

    Both remove...()s working

--- PE3/hello.c
+++ PE3/hello.c
@@ -21,13 +21,14 @@ void add_to_front(void* data, list* ls)
 {
     node* n = (node*)calloc(1, sizeof(node));   // allocate memory for the node
     
-    n->data = data; // set the new node's data member to the data argument
-    n->next = ls->head; // point the new node forward to the old head
-    if (ls->head == NULL)
-        ls->tail = n;   // set list's tail pointer to the new node
+    n->data = data;         // set the new node's data member to the data argument
+    n->next = ls->head;     // point the new node forward to the old head
+    n->prev = NULL;         // point the new node backward to NULL
+    if (!ls->head)
+        ls->tail = n;       // set list's tail pointer to the new node
     else
         ls->head->prev = n; // point the old head backward to the new node
-    ls->head = n;   // set list's head pointer to the new node
+    ls->head = n;           // set list's head pointer to the new node
 }
 
 /* */
@@ -35,13 +36,30 @@ void add_to_back(void* data, list* ls)
 {
     node* n = (node*)calloc(1, sizeof(node));   // allocate memory for the node
     
-    n->data = data; // set the new node's data member to the data argument
-    n->prev = ls->tail; // point the new node backward to the old tail
-    if (ls->head == NULL)
-        ls->head = n;   // set list's head pointer to the new node
+    n->data = data;         // set the new node's data member to the data argument
+    n->prev = ls->tail;     // point the new node backward to the old tail
+    n->next = NULL;         // point the new node forward to NULL
+    if (!ls->head)
+        ls->head = n;       // set list's head pointer to the new node
     else
         ls->tail->next = n; // point the old tail forward to the new node
-    ls->tail = n;   // set list's tail pointer to the new node
+    ls->tail = n;           // set list's tail pointer to the new node
+}
+
+void* remove_from_front(list* ls)
+{
+    void* data = ls->head->data; // cache the data of the node to remove
+    free(ls->head);              // free the memory used by the removed node
+    ls->head = ls->head->next;   // set list's head pointer to the next node
+    return data;
+}
+
+void* remove_from_back(list* ls)
+{
+    void* data = ls->tail->data; // cache the data of the node to remove
+    free(ls->tail);              // free the memory used by the removed node
+    ls->tail = ls->tail->prev;   // set list's tail pointer to the prev node
+    return data;
 }
 
 /* */
@@ -51,21 +69,16 @@ int main()
     node* head = (node*)calloc(1, sizeof(node));
     node* tail = (node*)calloc(1, sizeof(node));
     
-    head->data = (int*)1;
-    head->prev = NULL;
-    head->next = tail;
+    add_to_front((int*)1, ls);
+    add_to_front((int*)2, ls);
+    add_to_front((int*)3, ls);
 
-    tail->data = (int*)2;
-    tail->prev = head;
-    tail->next = NULL;
-
-    //ls->head = head;
-    //ls->tail = tail;
-
-
-    add_to_back((int*)3, ls);
-
-    printf("%i, %i\n", ls->head->data, ls->tail->data);
+    printf("%i, %i, %i\n", ls->head->data, ls->head->next->data, ls->head->next->next->data);
+    printf("Removed %i\n", remove_from_back(ls));
+    printf("%i, %i\n", ls->head->data, ls->head->next->data);
+    printf("Removed %i\n", remove_from_back(ls));
+    printf("%i\n", ls->head->data);
+    printf("Removed %i\n", remove_from_back(ls));
 
     return(0);
 }
