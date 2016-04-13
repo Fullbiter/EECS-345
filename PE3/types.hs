d606e080307b2f55f50e1cbc8b614674d238aad1
Kevin Nash <kjn33@case.edu>
2016-04-12 22:58:58 -0400

    Flatten works

--- PE3/types.hs
+++ PE3/types.hs
@@ -1,1 +1,11 @@
-data Bool = False | True 
+
+data Elements a = Element a | SubList [Elements a] deriving (Show, Eq)
+
+flatten :: [Elements a] -> [Elements a]
+
+flatten [Element a] = [Element a]
+flatten [SubList a] = (flatten a)
+flatten [] = []
+flatten ls = (flatten [(head ls)]) ++ (flatten (tail ls))
+
+
