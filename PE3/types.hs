d35e4803edea85ec8749c8c39a46db4d7f1e5f6a
Kevin Nash <kjn33@case.edu>
2016-04-12 23:19:56 -0400

    Finished Part 2

--- PE3/types.hs
+++ PE3/types.hs
@@ -3,9 +3,14 @@ data Elements a = Element a | SubList [Elements a] deriving (Show, Eq)
 
 flatten :: [Elements a] -> [Elements a]
 
-flatten [Element a] = [Element a]
 flatten [SubList a] = (flatten a)
+flatten [Element a] = [Element a]
 flatten [] = []
 flatten ls = (flatten [(head ls)]) ++ (flatten (tail ls))
 
+myreverse :: [Elements a] -> [Elements a]
 
+myreverse [SubList a] = [SubList (myreverse a)]
+myreverse [Element a] = [Element a]
+myreverse [] = []
+myreverse ls = (myreverse (tail ls)) ++ (myreverse [(head ls)])
